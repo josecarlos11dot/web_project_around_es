d595fbe63248ea87ba8c9c6015fbb4f988533a27
José Arzate <arzajose@gmail.com>
2025-07-16 22:53:18 -0600

    Finalizada sección gallery: funcionalidad like, estilos y merge con popup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Abrir y cerrar el popup
+
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup_type_edit');
 const closeButton = popup.querySelector('.popup__close');
@@ -12,15 +12,14 @@ closeButton.addEventListener('click', () => {
 });
 
 
-// === Validación del formulario y activación del botón "Guardar" ===
 
-// Selección de elementos
+
+
 const editForm = document.forms['edit-form'];
 const nameInput = editForm.elements['name'];
 const descriptionInput = editForm.elements['description'];
 const submitButton = editForm.querySelector('.popup__button');
 
-// Función para validar campos en tiempo real
 function toggleSubmitButton() {
   const isNameValid = nameInput.value.trim().length >= 2 && nameInput.validity.valid;
   const isDescriptionValid = descriptionInput.value.trim().length >= 2 && descriptionInput.validity.valid;
@@ -28,14 +27,22 @@ function toggleSubmitButton() {
   submitButton.disabled = !(isNameValid && isDescriptionValid);
 }
 
-// Escucha eventos en los inputs
+
 nameInput.addEventListener('input', toggleSubmitButton);
 descriptionInput.addEventListener('input', toggleSubmitButton);
 
-// Asegura que el botón esté desactivado si se resetea el formulario
 editForm.addEventListener('reset', () => {
   submitButton.disabled = true;
 });
 
-// Al cargar la página, valida el estado inicial
+
 window.addEventListener('DOMContentLoaded', toggleSubmitButton);
+
+
+const botonesLike = document.querySelectorAll('.gallery__like-button');
+
+botonesLike.forEach((boton) => {
+  boton.addEventListener('click', () => {
+    boton.classList.toggle('gallery__like-button_active');
+  });
+});
