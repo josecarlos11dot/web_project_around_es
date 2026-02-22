83d6e2b12cce13039733dd3f847f2b8588e3901e
José Carlos <arzajose@gmail.com>
2026-02-22 14:47:50 -0600

    fix: editar perfil y agregar tarjeta funcional sprint 9

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,22 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup_type_edit');
 const closeButton = popup.querySelector('.popup__close');
 
+
+const profileTitle = document.querySelector('.profile__name');       
+const profileDescription = document.querySelector('.profile__profession'); 
+const editForm = popup.querySelector(".popup__form");
+const nameInput = editForm.querySelector('input[name="name"]');
+const jobInput = editForm.querySelector('input[name="description"]');
+
+
+
+
+
 editButton.addEventListener('click', () => {
-  popup.classList.add('popup_opened');
+ nameInput.value = profileTitle.textContent;
+ jobInput.value = profileDescription.textContent;
+ 
+ popup.classList.add('popup_opened');
 });
 
 closeButton.addEventListener('click', () => {
@@ -40,8 +54,7 @@ function handleEscClose(evt) {
 
 
 
-const profileTitle = document.querySelector('.profile__name');       
-const profileDescription = document.querySelector('.profile__profession'); 
+
 
 
 
@@ -56,6 +69,12 @@ const addButton = document.querySelector('.profile__add-button');
 
 const addPopup = document.querySelector('.popup_type_add');
 
+const addForm = addPopup.querySelector(".popup__form");
+const titleInput = addForm.querySelector('input[name="title"]');
+const linkInput = addForm.querySelector('input[name="link"]');
+
+const galleryList = document.querySelector(".gallery__list");
+
 addButton.addEventListener('click', () => {
   addPopup.classList.add('popup_opened');
   
@@ -66,6 +85,34 @@ addPopup.querySelector('.popup__close').addEventListener('click', () => {
   closePopup(addPopup);
 });
 
+addForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const card = document.createElement("li");
+  card.classList.add("gallery__item");
+
+  card.innerHTML = `
+    <img src="${linkInput.value}" alt="${titleInput.value}" class="gallery__image">
+    <div class="gallery__footer">
+      <h2 class="gallery__title">${titleInput.value}</h2>
+      <button class="gallery__like-button"></button>
+    </div>
+  `;
+
+  galleryList.prepend(card);
+
+  closePopup(addPopup);
+});
+
+
+editForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  closePopup(popup);
+} )
 
 popup.addEventListener('mousedown', handleOverlayClick);
 addPopup.addEventListener('mousedown', handleOverlayClick);
