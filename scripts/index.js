801ceaf045f2039fc5ed19f588042b7627f5717a
José Arzate <arzajose@gmail.com>
2026-03-03 22:02:27 -0600

    refactor: clean index.js structure and finalize sprint 10 architecture

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
+// imports 
 import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
-
+// config
 const validationConfig = {
   inputSelector: ".popup__input", 
   submitButtonSelector: ".popup__button",
 };
 
 
-
+// selectors
+// profile
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup_type_edit');
 const closeButton = popup.querySelector('.popup__close');
@@ -17,23 +19,23 @@ const editForm = popup.querySelector(".popup__form");
 const nameInput = editForm.querySelector('input[name="name"]');
 const jobInput = editForm.querySelector('input[name="description"]');
 
-
+// add card
 const addButton = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('.popup_type_add');
 const addForm = addPopup.querySelector(".popup__form");
 const titleInput = addForm.querySelector('input[name="title"]');
 const linkInput = addForm.querySelector('input[name="link"]');
 
-
+// gallery
 const galleryList = document.querySelector(".gallery__list");
 
-
+// image popup
 const imagePopup = document.querySelector(".popup_type_image");
 const popupImage = imagePopup.querySelector(".popup__image");
 const popupCaption = imagePopup.querySelector(".popup__caption");
 const imagePopupClose = imagePopup.querySelector(".popup__close");
 
-
+// initialcards
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -61,20 +63,7 @@ const initialCards = [
   }
 ];
 
-
-editButton.addEventListener('click', () => {
- nameInput.value = profileTitle.textContent;
- jobInput.value = profileDescription.textContent;
- 
- popup.classList.add('popup_opened');
-});
-
-closeButton.addEventListener('click', () => {
-  closePopup(popup);
-});
-
-
-
+// functions
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_opened');
 
@@ -118,16 +107,50 @@ function createCardElement(data) {
   return card.generateCard();
 }
 
+// instances
+const editFormValidator = new FormValidator(validationConfig, editForm);
+editFormValidator.enableValidation();
+
+const addFormValidator = new FormValidator(validationConfig, addForm);
+addFormValidator.enableValidation();
+
+//event listeners
+// open popups
+
+
+editButton.addEventListener('click', () => {
+ nameInput.value = profileTitle.textContent;
+ jobInput.value = profileDescription.textContent;
+ 
+ popup.classList.add('popup_opened');
+});
 
 addButton.addEventListener('click', () => {
   addPopup.classList.add('popup_opened');
   
 });
-
+// close popups
+closeButton.addEventListener('click', () => {
+  closePopup(popup);
+});
 
 addPopup.querySelector('.popup__close').addEventListener('click', () => {
   closePopup(addPopup);
 });
+
+imagePopupClose.addEventListener("click", () => {
+  closePopup(imagePopup);
+});
+
+// submit forms
+editForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  closePopup(popup);
+} )
 
 addForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
@@ -142,28 +165,15 @@ addForm.addEventListener("submit", (evt) => {
   closePopup(addPopup);
 });
 
-
-editForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-
-  profileTitle.textContent = nameInput.value;
-  profileDescription.textContent = jobInput.value;
-
-  closePopup(popup);
-} )
-
+// global events
 popup.addEventListener('mousedown', handleOverlayClick);
 addPopup.addEventListener('mousedown', handleOverlayClick);
-
 imagePopup.addEventListener('mousedown', handleOverlayClick);
-
-imagePopupClose.addEventListener("click", () => {
-  closePopup(imagePopup);
-});
 
 document.addEventListener('keydown', handleEscClose);
 
 
+// initial render
 initialCards.forEach((item) => {
   const cardElement = createCardElement(item);
   galleryList.append(cardElement);
